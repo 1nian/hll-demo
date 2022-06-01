3dab97732688eab8e956936b5c1271033e1354f6
zs.yu <zs.yu@herin.ai>
2022-06-01 11:58:22 +0800

    解决重复点击路由报错

--- src/router/index.js
+++ src/router/index.js
@@ -49,6 +49,15 @@ const routes = [
   
 ]
 
+
+// 解决重复点击路由报错
+
+const originalPush = VueRouter.prototype.push
+VueRouter.prototype.push = function push (location) {
+    return originalPush.call(this, location).catch(err => err)
+} 
+
+
 const router = new VueRouter({
   routes,
   linkActiveClass: 'nav-active',
