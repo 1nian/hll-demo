d1b53e3cebf681fceca2d1c5a5b5c02045a70d9e
zs.yu <zs.yu@herin.ai>
2022-06-02 18:26:20 +0800

    打包优化

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,13 @@
-const { defineConfig } = require('@vue/cli-service')
+const { defineConfig } = require('@vue/cli-service');
+
+// const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+
+  // 项目优化可视化
+  // configureWebpack: {
+  //   plugins: [
+  //     new BundleAnalyzerPlugin()
+  //   ]
+  // }
 })
