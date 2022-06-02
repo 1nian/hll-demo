bc2f61a70bfeaa2b0d731666204f1a95e65a311d
zs.yu <zs.yu@herin.ai>
2022-06-02 15:36:48 +0800

    完善表格搜索功能

--- src/components/HllSearch.vue
+++ src/components/HllSearch.vue
@@ -1,34 +1,59 @@
 <template>
   <div class="hll-search">
-    <el-input class="hll-search-input" v-model="input" :placeholder="placeholder"></el-input>
-    <el-button class="hll-el-button">主要按钮</el-button>
-    <el-button>重置</el-button>
+    <el-input
+      class="hll-search-input"
+      v-model="value"
+      :placeholder="placeholder"
+    ></el-input>
+    <el-button class="hll-el-button hll-search-button" @click="serach"
+      >搜索</el-button
+    >
+    <el-button v-show="isClear" class="hll-search-button" @click="clear"
+      >重置</el-button
+    >
   </div>
 </template>
 
 <script>
 export default {
-  props:{
+  props: {
     // 搜索条件
-    placeholder:{
-      type:String,
-      default:'搜索文本框'
-    }
+    placeholder: {
+      type: String,
+      default: "搜索文本框",
+    },
   },
-  data(){
+  data() {
     return {
-      input:''
-    }
-  }
+      value: "",
+      isClear: false,
+    };
+  },
+  methods: {
+    serach() {
+      if (this.value) {
+        this.isClear = true;
+        this.$emit("searchData", this.value);
+      }
+    },
+    clear() {
+      this.value = "";
+      this.isClear = false;
+      this.$emit("searchData", null);
+    },
+  },
 };
 </script>
 
 <style>
-.hll-search{
+.hll-search {
   display: flex;
 }
-.hll-search-input{
+.hll-search-input {
   width: 260px;
   margin-right: 10px;
+}
+.hll-search-button {
+  width: 88px;
 }
 </style>
--- src/views/security/Equipment.vue
+++ src/views/security/Equipment.vue
@@ -1,60 +1,84 @@
 <template>
   <div class="security-equipment">
-    <hll-search></hll-search>
-    <hll-table :titleData="titleData" :tableData="filterTableData"
-    :currentPage="currentPage" :pageSize="pageSize" :total="total"
-    @sizeChange="getSizeChange" @currentChange="getCurrentChange"
+    <hll-search
+      @searchData="queryTableData"
+      placeholder="请输入名称"
+    ></hll-search>
+    <hll-table
+      :titleData="titleData"
+      :tableData="filterTableData"
+      :currentPage="currentPage"
+      :pageSize="pageSize"
+      :total="total"
+      @sizeChange="getSizeChange"
+      @currentChange="getCurrentChange"
     ></hll-table>
   </div>
 </template>
 
 <script>
-import HllTable from '../../components/HllTable.vue'
-import HllSearch from '../../components/HllSearch.vue'
-import {getList} from '../../api/getData'
+import HllTable from "../../components/HllTable.vue";
+import HllSearch from "../../components/HllSearch.vue";
+import { getList } from "../../api/getData";
 export default {
   name: "SecurityEquipment",
-  components:{
+  components: {
     HllTable,
-    HllSearch
+    HllSearch,
   },
   data() {
     return {
       tableData: [],
+      alwaysData: [],
       titleData: [
         { label: "名称", prop: "name" },
         { label: "日期", prop: "date" },
         { label: "地址", prop: "address" },
       ],
-      currentPage:0,
-      pageSize:10,
-      total:0,
+      currentPage: 0,
+      pageSize: 10,
+      total: 0,
     };
   },
-  computed:{
-    filterTableData(){
-      return this.tableData.slice(this.currentPage,(this.pageSize+this.currentPage))
-    }
+  computed: {
+    filterTableData() {
+      return this.tableData.slice(
+        this.currentPage,
+        this.pageSize + this.currentPage
+      );
+    },
   },
-  mounted(){
-    this.getTableData()
+  mounted() {
+    this.getTableData();
   },
-  methods:{
-    async getTableData(){
+  methods: {
+    async getTableData() {
       const res = await getList();
-      if(res.data.code === 200){
-        this.tableData = res.data.data.data
-        this.total = res.data.total
+      if (res.data.code === 200) {
+        this.tableData = res.data.data.data;
+        this.alwaysData = res.data.data.data;
+        this.total = res.data.data.total;
       }
     },
-    getSizeChange(val){
+    getSizeChange(val) {
       this.pageSize = val;
       this.currentPage = 1;
     },
-    getCurrentChange(val){
+    getCurrentChange(val) {
       this.currentPage = val;
     },
-  }
+    queryTableData(item) {
+      let data = JSON.parse(JSON.stringify(this.alwaysData));
+
+      if(item){
+        this.tableData = data.filter(data => data.name.toLowerCase().includes(item.toLowerCase()));
+        this.total = this.tableData.length;
+      }else{
+        this.tableData = this.alwaysData;
+        this.total = this.alwaysData.length;
+      }
+    },
+  },
 };
 </script>
 
